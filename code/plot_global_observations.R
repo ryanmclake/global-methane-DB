67f21fa70182f0cd12d4ef30798b71914228d31e
ryanmclake <ryan333@vt.edu>
2023-01-19 11:15:20 -0700

    update data

--- code/plot_global_observations.R
+++ code/plot_global_observations.R
@@ -10,6 +10,42 @@ library(rnaturalearthdata)
 library(viridis)
 library(tidyverse)
 
+library(sp)
+library(rworldmap)
+
+# The single argument to this function, points, is a data.frame in which:
+#   - column 1 contains the longitude in degrees
+#   - column 2 contains the latitude in degrees
+coords2country = function(points)
+{  
+  countriesSP <- getMap(resolution='low')
+  #countriesSP <- getMap(resolution='high') #you could use high res map from rworldxtra if you were concerned about detail
+  
+  # convert our list of points to a SpatialPoints object
+  
+  # pointsSP = SpatialPoints(points, proj4string=CRS(" +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"))
+  
+  #setting CRS directly to that from rworldmap
+  pointsSP = SpatialPoints(points, proj4string=CRS(proj4string(countriesSP)))  
+  
+  
+  # use 'over' to get indices of the Polygons object containing each point 
+  indices = over(pointsSP, countriesSP)
+  
+  # return the ADMIN names of each country
+  indices$ADMIN  
+  #indices$ISO3 # returns the ISO3 code 
+  #indices$continent   # returns the continent (6 continent model)
+  #indices$REGION   # returns the continent (7 continent model)
+}
+
+d <- vroom::vroom("./data/organized_data_to_append/global_lake_res_DB.csv")
+points = data.frame(lon=c(d$lon), lat=c(d$lat))
+
+countries <- data.frame(coords2country(points)) %>%
+  write_csv("./data/countries.csv")
+
+length(unique(coords2country(points)))
 
 # Load in the dataset
 d1_diff <- vroom::vroom("./data/organized_data_to_append/global_lake_res_DB.csv") %>%
@@ -31,6 +67,7 @@ obs_diffusive_flux <-
   ggplot() +
   geom_sf(data = world, lwd = 0.3, color = "black", fill = "white")+
   xlab("Longitude") + ylab("Latitude") +
+  labs(title = paste0("    N diffusive observations = ", length(d1_diff$ch4_diff)))+
   geom_sf(data = d1_diff, pch = 21, size = 3, color = "black", aes(fill = sqrt(ch4_diff)))+
   scale_fill_gradient2(midpoint=0, low="tan1", mid="dodgerblue4",
                        high="red", space ="Lab", na.value="grey",
@@ -48,6 +85,7 @@ obs_ebullition_flux <-
   ggplot() +
   geom_sf(data = world, lwd = 0.3, color = "black", fill = "white")+
   xlab("Longitude") + ylab("Latitude") +
+  labs(title = paste0("    N ebullition observations = ", length(d1_ebu$ch4_ebu)))+
   geom_sf(data = d1_ebu, pch = 21, size = 3, color = "black", aes(fill = sqrt(ch4_ebu)))+
   scale_fill_gradient2(midpoint=0, low="tan1", mid="dodgerblue4",
                        high="red", space ="Lab", na.value="grey",
